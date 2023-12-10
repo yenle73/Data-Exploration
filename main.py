7734bafbcad02dc2085e765fff996f28d121ab2e
yenle73 <128300242+yenle73@users.noreply.github.com>
2023-12-10 12:30:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
+import streamlit as st
+import pandas as pd
+import io
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
+st.title("Data Exploration")
+
+st.header('Uploading data')
+data_file = st.file_uploader("Choose a file", type=(['.csv']))
+
+st.header('Visualizing data')
+if data_file is not None:
+  df = pd.read_csv(data_file)
+  st.dataframe(df)
+  
+  st.header('Descriptive Statistics')
+  st.table(df.describe())
+
+  st.header('Data Information')
+  buffer = io.StringIO()
+  df.info(buf=buffer)
+  st.text(buffer.getvalue())
+
+  st.header('Visualize each attribute')
+  for col in list(df.columns):
+    fig, ax = plt.subplots()
+    ax.hist(df[col], bins=20)
+    plt.xlabel(col)
+    plt.ylabel('Quanity')
+    st.pyplot(fig)
+
+  st.header('Covariance')
+  correlation_matrix = np.corrcoef(df, rowvar=False)
+  fig, ax = plt.subplots()
+  sns.heatmap(correlation_matrix, annot=True, cmap='Reds', fmt=".2f", ax=ax)
+  st.pyplot(fig)
+
+  
+  st.header('Relationship between variable')
+  
+  output = st.radio('Chooses a dependent variable', df.columns)
+  for col in list(df.columns):
+    if col != output:
+      fig, ax = plt.subplots()
+      ax.scatter(x=df[col], y=df[output])
+      plt.xlabel(col)
+      plt.ylabel(output)
+      st.pyplot(fig)
